f1041b6bd5a9016b201fae2396faac6f07ac4242
Afreen Naz <afreen.naz900@gmail.com>
2022-05-13 02:36:03 +0100

    integrated sign in api

--- src/components/SignIn/SignInComponent.vue
+++ src/components/SignIn/SignInComponent.vue
@@ -2,8 +2,7 @@
 
     <div class="mt-5">
 
-        <b-card title="Sign In" class="mb-2  card-custom-style" 
-        >
+        <b-card title="Sign In" class="mb-2  card-custom-style">
 
 
             <b-card-body class="p-4">
@@ -11,40 +10,31 @@
                 <div class="float-left">
                     Email
                 </div>
-                <b-input
-                    placeholder="Enter your email here.."
-                    type="email"
-                    v-model="email"
-                >
+                <b-input placeholder="Enter your email here.." type="email" v-model="email">
                 </b-input>
 
                 <div class="float-left mt-2">
                     Password
                 </div>
-                <b-input
-
-                    placeholder="Enter your password here.."
-                    type="password"
-                    v-model="password"
-                >
+                <b-input placeholder="Enter your password here.." type="password" v-model="password">
                 </b-input>
 
-                <a class="float-right mt-1 text-danger ">
-                    <span class="text-danger">
+                <span class="text-danger" v-if="showError">
+                    {{ errorMsg }}
+                </span>
+                
+                <a class="float-right mt-1 text-secondary ">
+                    <span class="text-secondary">
                         Forgot password?
                     </span>
                 </a>
 
 
-                <b-button class="mt-5" block variant="outline-primary"
-                    @click="handle_user_signin"
-                >Sign In</b-button>
-    
-                <b-button class="mt-3" block variant="outline-info"
-                    @click="handle_user_signup"
-                >Sign Up</b-button>
-    
-        </b-card-body>
+                <b-button class="mt-5" block variant="outline-primary" @click="handle_user_signin">Sign In</b-button>
+
+                <b-button class="mt-3" block variant="outline-info" @click="handle_user_signup">Sign Up</b-button>
+
+            </b-card-body>
 
         </b-card>
 
@@ -55,23 +45,59 @@
 
 <script>
 
-export default{
-    name : "SignInComponent",
-    data(){
+
+const axios = require('axios')
+
+export default {
+    name: "SignInComponent",
+    data() {
         return {
-            email : "",
-            password : "",
+            email: "",
+            password: "",
+            showError : false,
+            errorMsg : "",
 
         }
     },
 
-    methods : {
-        handle_user_signin : function(){
-            this.$router.push({path : "/dashboard"})
+    methods: {
+        handle_user_signin: async function () {
+
+            if (this.email !== "" && this.password !== "") {
+                let response = await this.handle_sign_in_request();
+                
+                if ( response.data.error == 0){
+                    this.$router.push({path : "/dashboard"})
+                } else {
+                    this.showError = true;
+                    this.errorMsg = response.data.message
+                }
+            }
         },
 
-        handle_user_signup : function(){
-            this.$router.push({path : "/signup"})
+        handle_user_signup: function () {
+            this.$router.push({ path: "/signup" })
+        },
+
+        handle_sign_in_request: async function () {
+
+            var user = new FormData()
+            user.append('code', 'mufxVRh3gEVnXk1BaNnp5L5zi0Xiv3dKn46058LRoPvbAzFuf06ECA%3D%3D')
+            user.append("email", this.email)
+            user.append("password", this.password)
+            console.log( user.get('email') )
+            let signInApi = "https://livehealthyfunctions.azurewebsites.net/api/signinfunction?code=mufxVRh3gEVnXk1BaNnp5L5zi0Xiv3dKn46058LRoPvbAzFuf06ECA%3D%3D&email=" + this.email + "&password=" + this.password;
+            let response = await axios({
+                method: "get",
+                url: signInApi,
+                data: user,
+                headers: {
+                    // "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+            })
+            
+            return response
 
         }
     }
@@ -81,13 +107,14 @@ export default{
 </script>
 
 <style>
-
 .card-custom-style {
     max-width: 50%;
-     margin: 0 auto; /* Added */
-    float: none; /* Added */
-    margin-bottom: 10px; /* Added */
+    margin: 0 auto;
+    /* Added */
+    float: none;
+    /* Added */
+    margin-bottom: 10px;
+    /* Added */
 
 }
-
 </style>
